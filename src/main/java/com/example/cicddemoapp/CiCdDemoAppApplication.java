6b1bb8c73c0ae365e1e9e74c3147ea4e60e8dbb7
seunrae <oluseun.ademilua@gmail.com>
2022-11-27 21:27:58 +0100

    Ãinit

---
+++ src/main/java/com/example/cicddemoapp/CiCdDemoAppApplication.java
@@ -0,0 +1,20 @@
+package com.example.cicddemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdDemoAppApplication.class, args);
+    }
+
+}
+//
+//<!--        git init-->
+//<!--        git add README.md-->
+//<!--        git commit -m "first commit"-->
+//<!--        git branch -M main-->
+//<!--        git remote add origin https://github.com/seunrae/ci-cd-project.git-->
+//<!--        git push -u origin main-->
